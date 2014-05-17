736716482cba38ed4fafa119e2e06a5d8c3d398e
Han Xu <hgcrpd@gmail.com>
2014-05-18 00:54:19 +0800

    file_wrapper for scrapy JSON output

--- app/raw/loader.py
+++ app/raw/loader.py
@@ -1,10 +1,12 @@
 """
 Helpers to load the Scrapy JSON output into RawModels
 """
+import json
 
 
 def file_wrapper(fp):
     """
     Yields parsed JSON objects from a line separated JSON file
     """
-    pass
+    for line in fp:
+        yield json.loads(line)
