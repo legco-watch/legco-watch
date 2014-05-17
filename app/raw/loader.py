3c6a6718f8d11eef76368b90c74329e55ae91029
Han Xu <hgcrpd@gmail.com>
2014-05-18 00:43:31 +0800

    Tested provisioning script for celery, start loaders for raw models

---
+++ app/raw/loader.py
@@ -0,0 +1,10 @@
+"""
+Helpers to load the Scrapy JSON output into RawModels
+"""
+
+
+def file_wrapper(fp):
+    """
+    Yields parsed JSON objects from a line separated JSON file
+    """
+    pass
