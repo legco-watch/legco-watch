80ca82c12e3c1ead0f5d4ab264dd18bc99dafaf0
Han Xu <hgcrpd@gmail.com>
2014-05-25 17:29:30 +0800

    Fix some deployment issues, test landing page

--- app/legcowatch/urls.py
+++ app/legcowatch/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from django.views.generic import TemplateView
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'legcowatch.views.home', name='home'),
+    url(r'^$', TemplateView.as_view(template_name='landing.html'), name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
