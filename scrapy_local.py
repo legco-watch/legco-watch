b3850c475e449c0c6182629aa7521f335e86b1e1
Han Xu <hgcrpd@gmail.com>
2014-05-26 22:10:35 +0800

    Fix issue with scrapy local settings

--- scrapy_local.py
+++ scrapy_local.py
@@ -5,8 +5,5 @@
 import os
 
 # use this for running scrapy directly
-# PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-# FILES_STORE = os.path.join(PROJECT_ROOT, 'datafiles')
-
-# Use this for deploying to scrapyd, as it would be in stage/production
-FILES_STORE = '/var/lib/scrapyd/files'
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+FILES_STORE = os.path.join(PROJECT_ROOT, 'datafiles')
