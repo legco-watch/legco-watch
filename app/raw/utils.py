4aaedfeae122ed925895938f09ffe3e630127010
Han Xu <hgcrpd@gmail.com>
2014-05-28 21:23:08 +0800

    function for identifying file type

--- app/raw/utils.py
+++ app/raw/utils.py
@@ -3,9 +3,24 @@ Some utilities for working with spiders
 """
 from scrapy.crawler import Crawler
 from scrapy.utils.project import get_project_settings
+import magic
 
 
 def list_spiders():
     settings = get_project_settings()
     crawler = Crawler(settings)
     return crawler.spiders.list()
+
+
+def check_file_type(filepath):
+    filetype = magic.from_file(filepath)
+    if not filetype:
+        return 'Filetype Could Not Be Determined'
+    elif filetype == 'empty':
+        return 'Filetype Could Not Be Determined (file looks empty)'
+    elif filetype == 'very short file (no magic)':
+        return 'Filetype Could Not Be Determined (very short file)'
+    elif "Microsoft Office Word" in filetype:
+        return 'Microsoft Word Doc'
+    else:
+        return filetype
