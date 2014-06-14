14790f93c988d301c0b383ea751f596a87d31024
Han Xu <hgcrpd@gmail.com>
2014-06-14 23:16:33 +0800

    Some notes on exploring the html of council agendas

--- app/raw/docs/agenda.py
+++ app/raw/docs/agenda.py
@@ -44,8 +44,10 @@ class CouncilAgenda(object):
         pass
 
 """
+
 from raw import models, processors, utils
 import subprocess
+import pydocx
 # Gets agendas since Jan 2013
 objs = models.RawCouncilAgenda.objects.order_by('-uid').all()
 full_files = [processors.get_file_path(xx.local_filename) for xx in objs]
@@ -53,6 +55,28 @@ foo = [utils.check_file_type(xx, as_string=True) for xx in full_files]
 bar = zip(objs, foo, full_files)
 doc_e = bar[60]
 doc_c = bar[61]
+docx_e = bar[0]
+docx_c = bar[1]
+
+import lxml
+import re
+root = lxml.html.fromstring(utils.doc_to_html(doc_e[2]).encode('utf-8'))
+elems = root[1][0]
+text = elems.xpath('.//text()')
+pattern = r'^[IV]+\.'
+res = []
+for p in text:
+    if re.search(pattern, p):
+        res.append(p)
+# res now contains the text of the headers, use getparent() to get elements
+headers = [xx.getparent() for xx in res]
+# to find the location in the document, we can use iter()
+
+# clean the html
+from lxml.html.clean import clean_html
+clean_res = clean_html(res)
+
+# Write to local disk to testing
 res = utils.doc_to_html(doc_e[2])
 with open('doc_e.html', 'wb') as foo:
     foo.write(res.encode('utf-8'))
@@ -60,9 +84,6 @@ res = utils.doc_to_html(doc_c[2])
 with open('doc_c.html', 'wb') as foo:
     foo.write(res.encode('utf-8'))
 
-import pydocx
-docx_e = bar[0]
-docx_c = bar[1]
 res = pydocx.docx2html(docx_e[2])
 with open('docx_e.html', 'wb') as foo:
     foo.write(res.encode('utf-8'))
