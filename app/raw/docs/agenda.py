225bf5f55697b24a52f79a06f15014d3f115d634
Han Xu <hgcrpd@gmail.com>
2014-06-15 20:16:24 +0800

    WIP parsing of agenda document

--- app/raw/docs/agenda.py
+++ app/raw/docs/agenda.py
@@ -2,6 +2,9 @@
 Document wrappers for LegCo Agendas
 """
 from django.utils.functional import cached_property
+import lxml
+from lxml import etree
+import re
 
 
 class CouncilAgenda(object):
@@ -11,58 +14,82 @@ class CouncilAgenda(object):
     """
     def __init__(self, source, *args, **kwargs):
         self.source = source
+        self.tree = None
+        self.tabled_papers = None
+        self.other_papers = None
+        self.questions = None
+        self.motions = None
+        self.members_motions_on_legislation = None
+        self.members_bills = None
+        self.members_motions = None
+        self._load()
+        self._clean()
+        self._parse()
 
     def __repr__(self):
-        return '<CouncilAgenda: {}'.format(self.source)
+        return '<CouncilAgenda>'
 
-    @cached_property
-    def tabled_papers(self):
+    def _load(self):
+        """
+        Load the ElementTree from the source
+        """
+        # Convert directional quotation marks to regular quotes
+        double_quotes = ur'[\u201c\u201d]'
+        self.source = re.sub(double_quotes, u'"', self.source)
+        single_quotes = ur'[\u2019\u2018]'
+        self.source = re.sub(single_quotes, u"'", self.source)
+        # There are also some crazy "zero width joiners" in random places
+        # in the text
+        # &#8205, &#160 (nbsp)
+
+        self.tree = lxml.html.fromstring(self.source)
+
+    def _clean(self):
+        """
+        Removes some of extraneous tags to make parsing easier
+        """
+        etree.strip_tags(self.tree, 'strong')
+        for xx in self.tree.find_class('pydocx-tab'):
+            xx.drop_tag()
+
+    def _parse(self):
+        """
+        Parse the source document and populate this object's properties
+        """
         pass
 
-    @cached_property
-    def other_papers(self):
-        pass
+    def get_headers(self):
+        """
+        Gets the headers from the document
+        """
+        text = self.tree.xpath('//text()')
+        pattern = ur'^[IV]+\.'
+        res = []
+        for p in text:
+            if re.search(pattern, p):
+                res.append(p)
+        return res
 
-    @cached_property
-    def questions(self):
-        pass
 
-    @cached_property
-    def motions(self):
-        pass
+def load():
+    from raw import models, processors, utils
+    # Gets agendas since Jan 2013
+    objs = models.RawCouncilAgenda.objects.order_by('-uid').all()
+    full_files = [processors.get_file_path(xx.local_filename) for xx in objs]
+    foo = [utils.check_file_type(xx, as_string=True) for xx in full_files]
+    bar = zip(objs, foo, full_files)
+    doc_e = bar[60]
+    doc_c = bar[61]
+    docx_e = bar[0]
+    docx_c = bar[1]
+    source = utils.docx_to_html(docx_e[2])
+    agenda = CouncilAgenda(source)
+    return agenda
 
-    @cached_property
-    def members_motions_on_legislation(self):
-        pass
-
-    @cached_property
-    def members_bills(self):
-        pass
-
-    @cached_property
-    def members_motions(self):
-        pass
 
 """
 
-from raw import models, processors, utils
-import subprocess
-import pydocx
-# Gets agendas since Jan 2013
-objs = models.RawCouncilAgenda.objects.order_by('-uid').all()
-full_files = [processors.get_file_path(xx.local_filename) for xx in objs]
-foo = [utils.check_file_type(xx, as_string=True) for xx in full_files]
-bar = zip(objs, foo, full_files)
-doc_e = bar[60]
-doc_c = bar[61]
-docx_e = bar[0]
-docx_c = bar[1]
-
-import lxml
-import re
-root = lxml.html.fromstring(utils.doc_to_html(doc_e[2]).encode('utf-8'))
-elems = root[1][0]
-text = elems.xpath('.//text()')
+text = root.xpath('//text()')
 pattern = r'^[IV]+\.'
 res = []
 for p in text:
