f0c62a825822b3b1f217f34f7e9e4a297ea3074c
Han Xu <hgcrpd@gmail.com>
2014-07-06 23:08:35 +0800

    WIP member bio scraping

--- legcoscraper/spiders/members.py
+++ legcoscraper/spiders/members.py
@@ -22,11 +22,12 @@ class MemberBio(TypedItem):
     source_url = Field()
     title = Field()
     name = Field()
-    honorifics = Field()
+    honours = Field()
     gender = Field()
     year_of_birth = Field()
     place_of_birth = Field()
     homepage = Field()
+    service = Field()
     education = Field()
     occupation = Field()
     file_urls = Field()
@@ -41,10 +42,16 @@ class LibraryMemberSpider(Spider):
     KEYWORDS_E = {
         'basic_title': u'Basic information',
         'service_title': u'Period of LegCo service',
+        'education_title': u'Education and professional qualifications',
+        'occupation_title': u'Occupation',
         'field_map': {
             u'Title': 'title',
             u'Name': 'name',
-            u'Gender' : 'gender'
+            u'Honours': 'honours',
+            u'Gender': 'gender',
+            u'Year of birth': 'year_of_birth',
+            u'Place of birth': 'place_of_birth',
+            u'Personal homepage': 'homepage'
         }
     }
     KEYWORDS_C = {
@@ -94,7 +101,7 @@ class LibraryMemberSpider(Spider):
         # with 3 and 4 optional
 
         # Process basic info
-        if not tables[0].xpath('./tr[1]/td/text()').extract() == kws['basic_title']:
+        if not tables[0].xpath('./tr[1]/td/text()').extract()[0] == kws['basic_title']:
             logger.warn(u'First table not basic information')
             logger.warn(u'Failed parsing of {}'.format(response.url))
             return
@@ -102,11 +109,63 @@ class LibraryMemberSpider(Spider):
         fields = tables[0].xpath('.//table/tr')
         field_names = fields.xpath('./td[1]/text()').extract()
         field_values = fields.xpath('./td[2]/text()').extract()
+        # Copy over basic informtaion
+        for k, v in zip(field_names, field_values):
+            to_field = kws['field_name'].get(k, None)
+            if to_field is None:
+                logger.warn(u'Got unknown field {}'.format(k))
+                continue
+            res[to_field] = v
+
         # Get the image
         res['file_urls'] = tables[0].xpath('.//img/@src').extract()
 
         # Process service
-        if not tables[1].xpath('./tr[1]/td/text()').extract() == kws['service_title']:
+        if not tables[1].xpath('./tr[1]/td/text()').extract()[0] == kws['service_title']:
             logger.warn(u'Second table not LegCo service')
             logger.warn(u'Failed parsing of {}'.format(response.url))
             return
+
+        services = tables[1].xpath('./tr[position() > 1]')
+        service_list = []
+        for s in services:
+            service_list.append(s.xpath('./td/text()').extract())
+        res['service'] = service_list
+
+        # Next two tables are optional
+        occupation_table = None
+        education_table = None
+        if len(tables) == 3:
+            # If three tables, then only one of occupation or education
+            next_table_title = tables[2].xpath('./tr[1]/td/text()').extract()[0]
+            if next_table_title == kws['education_title']:
+                education_table = tables[2]
+            elif next_table_title == kws['occupation_title']:
+                occupation_table = tables[2]
+            else:
+                logger.warn(u'Could not identify table: {}'.format(next_table_title))
+        if len(tables) == 4:
+            # If four tables, then should be education then occupation
+            third_table = tables[2].xpath('./tr[1]/td/text()').extract()[0]
+            if not third_table == kws['education_title']:
+                logger.warn(u'Third table was not education: {}'.format(third_table))
+            else:
+                education_table = tables[2]
+            fourth_table = tables[3].xpath('./tr[1]/td/text()').extract()[0]
+            if not fourth_table == kws['occupation_title']:
+                logger.warn(u'Fourth table was not occupation: {}'.format(fourth_table))
+            else:
+                occupation_table = tables[3]
+
+        # Now process the optional tables if they're there
+        if education_table is not None:
+            educations = education_table.xpath('./tr[position() > 1]/td/text()').extract()
+            res['education'] = [xx.strip() for xx in educations]
+
+        if occupation_table is not None:
+            occupation = occupation_table.xpath('./tr[position() > 1]/td/text()').extract()
+            if len(occupation) > 1:
+                logger.warn(u'More than one occupation found: {}'.format(occupation))
+            res['occupation'] = occupation[0].strip()
+
+        yield MemberBio(**res)
