8b172879c10a13c2db828a25452694032241fa3d
Han Xu <hgcrpd@gmail.com>
2014-07-09 23:48:07 -0700

    WIP Agenda detail page

--- app/legcowatch/urls.py
+++ app/legcowatch/urls.py
@@ -11,7 +11,8 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', TemplateView.as_view(template_name='landing.html'), name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^raw/agendas', raw.views.RawCouncilAgendaListView.as_view(), name='raw_agenda_list'),
+    url(r'^raw/agendas/?$', raw.views.RawCouncilAgendaListView.as_view(), name='raw_agenda_list'),
+    url(r'^raw/agendas/(?P<pk>[0-9]+)/?$', raw.views.RawCouncilAgendaDetailView.as_view(), name='raw_agenda'),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
--- app/raw/views.py
+++ app/raw/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from raw.models import RawCouncilAgenda
 
 
@@ -9,3 +9,13 @@ class RawCouncilAgendaListView(ListView):
     model = RawCouncilAgenda
     template_name = 'raw/agenda_list.html'
     paginate_by = 25
+
+
+class RawCouncilAgendaDetailView(DetailView):
+    model = RawCouncilAgenda
+    template_name = 'raw/agenda_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(RawCouncilAgendaDetailView, self).get_context_data(**kwargs)
+        context['parser'] = self.object.get_parser()
+        return context
