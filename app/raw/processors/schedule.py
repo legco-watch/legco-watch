4861fc7af039abd6a30739ae79f4f69556e377a7
Han Xu <hgcrpd@gmail.com>
2014-07-21 22:08:32 -0700

    Refactor of processors for Schedule api

--- app/raw/processors/schedule.py
+++ app/raw/processors/schedule.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import logging
 import warnings
+
 from raw.models import RawScheduleMember
 from raw.processors.base import BaseProcessor, file_wrapper
 
@@ -8,19 +9,22 @@ from raw.processors.base import BaseProcessor, file_wrapper
 logger = logging.getLogger('legcowatch')
 
 
-class ScheduleMemberProcessor(BaseProcessor):
-    """
-    Class that handles the loading of Library Agenda scraped items into the RawCouncilAgenda table
-    """
+class BaseScheduleProcessor(BaseProcessor):
+    # Doing some refactoring, but don't want to affect other processors
+    model = None
+
     def process(self, *args, **kwargs):
         logger.info("Processing file {}".format(self.items_file_path))
         counter = 0
         for item in file_wrapper(self.items_file_path):
             counter += 1
-            self._process_item(item)
+            self._process_item_wrapper(item)
         logger.info("{} items processed, {} created, {} updated".format(counter, self._count_created, self._count_updated))
 
-    def _process_item(self, item):
+    def _process_item(self, item, obj):
+        raise NotImplementedError()
+
+    def _process_item_wrapper(self, item):
         uid = self._generate_uid(item)
         obj = self._get_object(uid)
         if obj is None:
@@ -29,24 +33,35 @@ class ScheduleMemberProcessor(BaseProcessor):
         obj.last_parsed = datetime.now()
         if self.job is not None:
             obj.last_crawled = self.job.completed
-
-        fields = ['last_name_c', 'first_name_c', 'last_name_e', 'first_name_e', 'english_name']
-        for f in fields:
-            setattr(obj, f, item.get(f, None))
-
-        obj.save()
+        self._process_item(item, obj)
 
     def _get_object(self, uid):
         try:
-            obj = RawScheduleMember.objects.get(uid=uid)
+            obj = self.model.objects.get(uid=uid)
             self._count_updated += 1
         except RawScheduleMember.DoesNotExist:
-            obj = RawScheduleMember(uid=uid)
+            obj = self.model(uid=uid)
             self._count_created += 1
-        except RawScheduleMember.MultipleObjectsReturned:
+        except self.model.MultipleObjectsReturned:
             warnings.warn("Found more than one item with raw id {}".format(uid), RuntimeWarning)
             obj = None
         return obj
+
+    def _generate_uid(self, item):
+        raise NotImplementedError()
+
+
+class ScheduleMemberProcessor(BaseScheduleProcessor):
+    """
+    Class that handles the loading of Library Agenda scraped items into the RawCouncilAgenda table
+    """
+    model = RawScheduleMember
+
+    def _process_item(self, item, obj):
+        fields = ['last_name_c', 'first_name_c', 'last_name_e', 'first_name_e', 'english_name']
+        for f in fields:
+            setattr(obj, f, item.get(f, None))
+        obj.save()
 
     def _generate_uid(self, item):
         return 'smember-{}'.format(item['id'])
