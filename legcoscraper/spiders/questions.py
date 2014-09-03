e29e94c61c3cd538a1e39501cd73092d45cf2a20
Han Xu <hgcrpd@gmail.com>
2014-09-03 22:18:45 +0100

    Working scraper for council questions

--- legcoscraper/spiders/questions.py
+++ legcoscraper/spiders/questions.py
@@ -4,7 +4,6 @@ Scraper for Council Meeting Questions
 
 From http://www.legco.gov.hk/yr13-14/english/counmtg/question/ques1314.htm#toptbl, for example
 """
-from django.utils.encoding import force_str
 import logging
 from lxml.html import HTMLParser
 from scrapy.item import Field
@@ -30,6 +29,7 @@ class Question(TypedItem):
 class QuestionSpider(Spider):
     name = 'council_question'
     start_urls = [
+        # English
         'http://www.legco.gov.hk/yr13-14/english/counmtg/question/ques1314.htm',
         'http://www.legco.gov.hk/yr12-13/english/counmtg/question/ques1213.htm',
         'http://www.legco.gov.hk/yr11-12/english/counmtg/question/ques1112.htm',
@@ -38,6 +38,15 @@ class QuestionSpider(Spider):
         'http://www.legco.gov.hk/yr08-09/english/counmtg/question/ques0809.htm',
         'http://www.legco.gov.hk/yr07-08/english/counmtg/question/ques0708.htm',
         'http://www.legco.gov.hk/yr06-07/english/counmtg/question/ques0607.htm',
+        # Chinese
+        'http://www.legco.gov.hk/yr13-14/chinese/counmtg/question/ques1314.htm',
+        'http://www.legco.gov.hk/yr12-13/chinese/counmtg/question/ques1213.htm',
+        'http://www.legco.gov.hk/yr11-12/chinese/counmtg/question/ques1112.htm',
+        'http://www.legco.gov.hk/yr10-11/chinese/counmtg/question/ques1011.htm',
+        'http://www.legco.gov.hk/yr09-10/chinese/counmtg/question/ques0910.htm',
+        'http://www.legco.gov.hk/yr08-09/chinese/counmtg/question/ques0809.htm',
+        'http://www.legco.gov.hk/yr07-08/chinese/counmtg/question/ques0708.htm',
+        'http://www.legco.gov.hk/yr06-07/chinese/counmtg/question/ques0607.htm',
         # These below no longer have links to replies
         # 'http://www.legco.gov.hk/yr05-06/english/counmtg/question/ques0506.htm',
         # 'http://www.legco.gov.hk/yr04-05/english/counmtg/question/ques0405.htm',
@@ -48,7 +57,8 @@ class QuestionSpider(Spider):
         # 'http://www.legco.gov.hk/yr99-00/english/counmtg/question/ques9900.htm',
         # 'http://www.legco.gov.hk/yr98-99/english/counmtg/question/question.htm'
     ]
-    HEADER_RE = u'Council meeting on (?P<date>[0-9.]+)'
+    HEADER_RE_E = u'Council meeting on (?P<date>[0-9.]+)'
+    HEADER_RE_C = u'會議日期\s*(?P<date>[0-9.]+)'
 
     def parse(self, response):
         sel = Selector(response)
@@ -60,21 +70,34 @@ class QuestionSpider(Spider):
         # Store the number of headers as a check for how many tables of questions we should get out
         # Some dates use h2 and others use h3 for the headers
         num_headers = len(body.xpath('./h2|./h3'))
+        if u'chinese' in response.url:
+            language = 'C'
+            matcher = self.HEADER_RE_C
+        else:
+            language = 'E'
+            matcher = self.HEADER_RE_E
         # We'll need lxml to parse this
         parser = HTMLParser(encoding='utf-8')
-        body_elements = lxml.html.fromstring(force_str(body.extract()), parser=parser)
+        body_extract = body.extract().encode('utf-8')
+        body_elements = lxml.html.fromstring(body_extract, parser=parser)
         # Iterate over the body elements, processing each h2-table pair for each meeting
-        count = 0
+        count_sessions = 0
+        count_questions = 0
         for elem in body_elements:
+            # Skip comments
+            if elem.tag == lxml.etree.Comment:
+                continue
             # Take the first 50 characters, so RE doesn't scan the whole body of text for large elements
-            match = re.search(self.HEADER_RE, elem.text_content()[:50])
+            match = re.search(matcher, elem.text_content()[:50])
             if match is not None:
                 this_date = match.groupdict()['date']
+                count_sessions += 1
                 questions_table = elem.getnext()
                 for row in questions_table:
                     # We ignore the header row, which is indicated by ths
                     if row[0].tag == 'th':
                         continue
+                    # Sometimes replies don't have links
                     this_question = {
                         'date': this_date,
                         'source_url': response.url,
@@ -82,6 +105,16 @@ class QuestionSpider(Spider):
                         'asker': row[1].text_content(),
                         'subject': row[2].text_content(),
                         'subject_link': row[2][0].get('href', None),
-                        'reply_link': row[3][0].get('href', None),
-                        'language': 'E'
+                        'language': language
                     }
+                    try:
+                        this_question['reply_link'] = row[3][0].get('href', None)
+                    except IndexError:
+                        logging.warn(u'No reply link on {} from {}'.format(response.url, this_date))
+                        pass
+                    yield Question(**this_question)
+                    count_questions += 1
+
+        if num_headers != count_sessions:
+            logging.warn(u'Expected {} sessions on {}, but only found tables for {}'.format(num_headers, response.url, count_sessions))
+        logging.info(u'Processed {} questions in {} sessions'.format(count_questions, count_sessions))
