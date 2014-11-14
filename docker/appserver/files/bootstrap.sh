1273000b9987ec3b69df14c6634df234bee75345
Han Xu <hgcrpd@gmail.com>
2014-11-14 12:35:16 +0800

    Working appserver container

--- docker/appserver/files/bootstrap.sh
+++ docker/appserver/files/bootstrap.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 source ${VIRTUALENV_PATH}/bin/activate
+
+echo "Pulling latest code and syncing database"
+    # If no command passed in, then pull code, migrate, and run the uwsgi server
+git pull --force
+cd app
+python manage.py syncdb --noinput
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+
 if [ $# -eq 0 ]
 then
-    # If no command passed in, then pull code, migrate, and run the uwsgi server
-    git pull --force
-    cd app
-    python manage.py syncdb --noinput
-    python manage.py migrate --noinput
-    python manage.py collectstatic --noinput
-
     ${VIRTUALENV_PATH}/bin/uwsgi --ini ${PROJECT_PATH}/legco-watch.ini
 else
     # This allows us to attach to the container
