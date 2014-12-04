7a7fa820272780acc2615004bbbab37fb90a5c24
Han Xu <hgcrpd@gmail.com>
2014-12-04 16:35:03 +0800

    Add boto, appdata container

--- docker/build.sh
+++ docker/build.sh
@@ -3,6 +3,7 @@
 echo "Building containers"
 docker build -t legcowatch/dbdata docker/dbdata
 docker build -t legcowatch/logdata docker/logdata
+docker build -t legcowatch/appdata docker/appdata
 docker build -t legcowatch/dbserver docker/dbserver
 docker build -t legcowatch/appserver docker/appserver
 docker build -t legcowatch/rabbitmq docker/rabbitmq
